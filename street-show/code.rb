254b427cd0f8f660ef1b23482ecc478a1bf226d6
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-09-10 17:47:15 -0400

    east digits

---
+++ street-show/code.rb
@@ -0,0 +1,21 @@
+# YOUR CODE GOES HERE
+
+Knife_Juggling = 11.87
+Torch_Juggling = 16.5
+Hand_Balancing = 3.00
+Human_Blockhead = 1.18
+
+
+sum = Knife_Juggling + Torch_Juggling + Hand_Balancing + Human_Blockhead
+
+average = sum/12
+
+
+
+
+puts "Hello everyone my assistant will help us run this show. Whats your name dude?"
+name = gets.chomp
+puts "ok cool interesting!kh and whats your fav numeber?"
+number = gets.chomp
+
+puts "sweet!"

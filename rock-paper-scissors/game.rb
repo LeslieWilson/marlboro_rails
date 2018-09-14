924d005ea8a737a3f27c3809edbfe7252650bba5
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-09-14 12:43:51 -0400

    rk paper scissors final activity of section

--- rock-paper-scissors/game.rb
+++ rock-paper-scissors/game.rb
@@ -1,22 +1,39 @@
+require 'pry'
+
 puts "this is a game of rock paper scissors. type 'start'"
 input = gets.chomp
 if input == "start"
-computer_roll_1 = rand(3)+1
-puts computer_roll_1
-end
+puts "player input your number! 1 for paper, 2 for scissors, 3 for rock"
 
-if computer_roll_1 == 1
-    puts "you got rock!"
-elsif computer_roll_1 == 2
-    puts "you got scissors!"
-elsif computer_roll_1 == 3
-    puts "you got paper!"
+
+player_roll = gets.chomp
+computer_roll = rand(3)+1
+
+end
+#
+while computer_roll == 1 && player_roll == 1
+    puts "you both got rock"
+
+
+if computer_roll == 1 && player_roll == 1
+    puts "#{player_roll}: #{computer_roll} comp got rock, player got rock, tie!"
+elsif computer_roll == 2 && player_roll == 2
+    puts "#{player_roll}: #{computer_roll} comp got scissors, player got scissors, tie!"
+elsif computer_roll == 3 && player_roll == 3
+    puts "#{player_roll}: #{computer_roll} you both got paper, tie!"
+elsif computer_roll == 1 && player_roll == 2
+    puts "#{player_roll}: #{computer_roll}, player got rock comp got scissors, player wins "
+elsif computer_roll == 1 && player_roll == 3
+    puts "#{player_roll}: #{computer_roll} player got rock comp got paper,comp wins! "
+elsif computer_roll == 3 && player_roll == 1
+    puts "#{player_roll}: #{computer_roll} comp paper, player rock, comp wins "
+else computer_roll == 3 && player_roll == 2
+    puts "#{player_roll}: #{computer_roll}comp paper, player scissors, player wins "
 end
 
 # elsif computer_roll_1 == 1
 #     puts "Rock"
 # end
-
 
 
 # elsif computer_roll == 2

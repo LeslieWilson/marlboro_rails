bc0e7063032a95fcfbf2d1c38abef1b48478e7fb
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-09-25 19:49:57 -0400

    basic drill

--- arrays/arrays.rb
+++ arrays/arrays.rb
@@ -1,2 +1,90 @@
-array_1 = ['michele', 'paul', 'mike']
-array_2 = ['michele paul mike']
+
+
+
+
+# ary = ["leslie", 'nate', 'dog']
+# i = 0
+# while i < array.length
+#     print array[i]
+#     i +=1
+#
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# puts "my to do list"
+# to_do = ["buy milk", "learn arrays", "learn ruby"]
+#
+# to_do.each do |item|
+#     print "YES"
+#     puts item
+# end
+
+# puts "my list of people i like"
+# array_1 = ['michele', 'paul', 'mike']
+# i = 0
+# while i < array_1.length
+#     print "*"
+#     puts array_1[i]
+#     i += 1
+# end

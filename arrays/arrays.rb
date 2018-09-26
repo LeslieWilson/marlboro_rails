7ade145f5fe1d22b1e6ce2bc35ca7029c9b5b1e3
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-09-26 16:52:07 -0400

    ternary expressions

--- arrays/arrays.rb
+++ arrays/arrays.rb
@@ -2,6 +2,67 @@
 
 
 
+
+
+
+#
+# ["bridget", "barney", "sam", "ella"].each {|name| puts (name.index('b')== 0)? "#{name} starts with b" : ""}
+#
+# ["bridget", "barney", "sam", "ella"].each do |name|
+#     if name.index('b') == 0
+#         puts "#{name} starts with 'b'"
+#     end
+# end
+#
+#
+#
+# names_to_greet = ['sally', 'sam', 'suzi']
+# names_to_greet.each { |name| puts "Hi #{name}"}
+#
+#
+#
+#
+#
+# numbers = [1,2,3,4,5]
+#
+#
+#
+# doubled_numbers = []
+#
+# numbers.each do |number|
+#     doubled_numbers << number * 2
+# end
+#
+#
+# doubled_numbers.each do |number|
+#     puts number
+# end
+#
+#
+#
+#
+# numbers.each do |number|
+#     puts number
+# end
+#
+#
+#
+# see above for .each version
+#
+# numbers = [1,2,3,4,5]
+# current_index = 0
+# max_index = numbers.length - 1
+#
+# while current_index <= max_index
+#     puts numbers[current_index]
+#     current_index += 1
+# end
+#
+#
+
+
+
+
 # ary = ["leslie", 'nate', 'dog']
 # i = 0
 # while i < array.length
@@ -9,69 +70,7 @@
 #     i +=1
 #
 # end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#
 # puts "my to do list"
 # to_do = ["buy milk", "learn arrays", "learn ruby"]
 #
@@ -79,7 +78,7 @@
 #     print "YES"
 #     puts item
 # end
-
+#
 # puts "my list of people i like"
 # array_1 = ['michele', 'paul', 'mike']
 # i = 0

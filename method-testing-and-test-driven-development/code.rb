e7345c0a9eafa4b144c4fb5ede0cb73997e5b00b
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-10-13 11:25:21 -0400

    test

--- method-testing-and-test-driven-development/code.rb
+++ method-testing-and-test-driven-development/code.rb
@@ -1,9 +1,14 @@
+def plus_one(number)
+    result = number + 1
+    return result
+end
+
 require_relative './code'
 
 describe 'plus_one' do
 it 'should be one plus the number given' do
 result = plus_one(0)
-expect (result).to eq(1)
+expect(result).to eq(1)
 
 end
 end

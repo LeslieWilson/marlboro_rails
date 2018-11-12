4274282f6e01bf770a993b71f7aef1b449d5179e
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-11-12 10:24:06 -0500

    testing setup

---
+++ methodTesting2/lib/bank_account.rb
@@ -0,0 +1,22 @@
+
+
+class BankAccount
+    attr_accessor :account_number, :initial_deposit, :transactions
+    def initialize(account_number, initial_deposit)
+        @account_number = account_number
+        @initial_deposit = initial_deposit
+        @transactions = []
+    end
+
+def add_transaction(amount)
+    @transactions << amount
+end
+
+def current_balance
+    result = initial_deposit
+    @transactions.each do |transaction|
+        result += transaction
+end
+return result
+end
+end
---
+++ methodTesting2/spec/lib/bank_account_spec.rb
@@ -0,0 +1,40 @@
+require "spec_helper"
+
+RSpec.describe BankAccount do
+    describe ".new" do
+        it "takes an account number and a deposit amount as arguments" do
+            bank_account = BankAccount.new(5406327, 1000.00)
+            expect(bank_account).to be_a(BankAccount)
+        end
+    end
+
+    describe "#account_number" do
+        it "returns the account number" do
+            bank_account = BankAccount.new(5406327, 1000.00)
+            expect(bank_account.account_number).to eq(5406327)
+        end
+
+    describe "#initial_deposit" do
+        it "returns the initial deposit" do
+            bank_account = BankAccount.new(5406327, 1000.00)
+            expect(bank_account.initial_deposit). to eq(1000)
+        end
+    describe "#add_transaction" do
+        it "stores the transaction in an array" do
+            bank_account = BankAccount.new(5406327, 1000.00)
+            bank_account.add_transaction(-5.49)
+            expect(bank_account.transactions).to include(-5.49)
+        end
+    describe "#current balance"do
+        it "calculates the current balance" do
+            bank_account = BankAccount.new(5406327, 1000.00)
+            bank_account.add_transaction(-5)
+            bank_account.add_transaction(-10)
+            bank_account.add_transaction(-20)
+            expect(bank_account.current_balance). to eq(965)
+        end
+    end
+    end
+    end
+    end
+end

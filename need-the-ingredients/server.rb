07451e59f62caa298fc9f1966c40ac9d153972f4
LeslieWilson <leslie.wilson.vt@gmail.com>
2018-12-01 19:46:28 -0500

    server practice

--- need-the-ingredients/server.rb
+++ need-the-ingredients/server.rb
@@ -1,26 +1,19 @@
 require 'sinatra'
 require "pry" if development? || test?
 require "sinatra/reloader" if development?
-
-
 set :bind, '0.0.0.0'
-
 get '/' do
-  @ingredients = File.readlines('ingredients.txt')
+  @ingredients = File.readlines("ingredients.txt")
   erb :index
 end
-
-get "/ingredients/:whatever" do
-    @ingredient = params[:whatever]
+get "/:whatever" do
+    @ingredient_name = params[:whatever]
 erb :show
 end
-
-
-post '/ingredients' do
-  ingredient = params['ingredient']
-  File.open('ingredients.txt', 'a') do |file|
+post "/ingredients" do
+  ingredient = params["ingredient_name"]
+  File.open("ingredients.txt", 'a') do |file|
     file.puts(ingredient)
   end
-
   redirect '/'
 end

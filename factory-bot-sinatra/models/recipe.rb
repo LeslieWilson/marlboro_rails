3b9a4af146fc3add250e2f777ecc85606b7fc816
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-01-11 16:59:49 -0500

    factory bot

--- factory-bot-sinatra/models/recipe.rb
+++ factory-bot-sinatra/models/recipe.rb
@@ -3,9 +3,8 @@ class Recipe < ActiveRecord::Base
   validates :instructions, presence: true
   validates :cooking_time, numericality: { greater_than: 0 }
   validates :servings, numericality: { greater_than: 0 }
-end
 
-
-def self.quick_recipes
-  where("cooking_time < ?", 30)
+  def self.quick_recipes
+    where("cooking_time < ?", 30)
+  end
 end
--- factory-bot-sinatra/spec/factories/recipes.rb
+++ factory-bot-sinatra/spec/factories/recipes.rb
@@ -1,4 +1,10 @@
 FactoryBot.define do
   factory :recipe do
+    name { "Green Eggs & Ham" }
+    description { "Would you eat them in a box? Would you eat them with a fox?" }
+    instructions { "Eggs + Ham + Food coloring" }
+    servings { 4 }
+    cooking_time { 45 }
+
   end
 end

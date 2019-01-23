2169cdf8c7355e0eeb2d89dab0a4ec8acf6b2aa8
LeslieWilson <leslie.wilson.vt@gmail.com>
2019-01-22 19:43:36 -0500

    working nested component

--- react-nested-components/src/components/Icon.js
+++ react-nested-components/src/components/Icon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Icon = props => {
-    let iconClass = "fa fa-2x fa-fw" + props.fontAwesomeSymbol;
+    let iconClass = "fa fa-2x fa-fw " + props.fontAwesomeSymbol;
     let iconName = props.iconName;
     let descriptionAlert = () => alert(props.description);
 
